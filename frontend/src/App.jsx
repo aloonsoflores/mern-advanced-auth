b9aa2ddfee341f621c72bba887a756979147e33e
aloonsoflores <floalonso.abad@gmail.com>
2025-04-21 14:17:17 +0200

    tailwind-colors change palette

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -55,19 +55,12 @@ function App() {
 
 	return (
 		<div
-			className={`min-h-screen flex items-center justify-center relative overflow-hidden
-      transition-colors duration-300
-      ${theme === 'dark'
-        ? 'bg-gradient-to-br from-gray-900 via-green-900 to-emerald-900'
-        : 'bg-gradient-to-br from-blue-100 via-blue-50 to-white'}`}
+			className='min-h-screen bg-gradient-to-br
+    from-gray-900 via-fuchsia-900 to-purple-900 flex items-center justify-center relative overflow-hidden'
 		>
-			<div className="absolute top-4 right-4">
-        <Toggle theme={theme} setTheme={setTheme} />
-      </div>
-
-			<FloatingShape color={theme === 'dark' ? 'bg-green-500' : 'bg-green-300'} size='w-64 h-64' top='-5%' left='10%' delay={0} />
-			<FloatingShape color={theme === 'dark' ? 'bg-emerald-500' : 'bg-emerald-300'} size='w-48 h-48' top='70%' left='80%' delay={5} />
-			<FloatingShape color={theme === 'dark' ? 'bg-lime-500' : 'bg-lime-300'} size='w-32 h-32' top='40%' left='-10%' delay={2} />
+			<FloatingShape color='bg-fuchsia-500' size='w-64 h-64' top='-5%' left='10%' delay={0} />
+			<FloatingShape color='bg-purple-500' size='w-48 h-48' top='70%' left='80%' delay={5} />
+			<FloatingShape color='bg-pink-500' size='w-32 h-32' top='40%' left='-10%' delay={2} />
 
 			<Routes>
 				<Route
